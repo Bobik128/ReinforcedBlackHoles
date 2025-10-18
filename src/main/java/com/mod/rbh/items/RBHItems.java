bbd82da2cd9a4140bfcbe95d64e3677533622521
tomas <bocan.tomas.6666@gmail.com>
2025-10-18 15:37:11 +0200

    create compat

--- src/main/java/com/mod/rbh/items/RBHItems.java
+++ src/main/java/com/mod/rbh/items/RBHItems.java
@@ -1,7 +1,6 @@
 package com.mod.rbh.items;
 
 import com.mod.rbh.ReinforcedBlackHoles;
-import com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModList;
@@ -19,13 +18,30 @@ public class RBHItems {
     public static final RegistryObject<Item> SINGULARITY_BATTERY_EMPTY = ITEMS.register("singularity_battery_empty",
             () -> new Item(new Item.Properties().stacksTo(16)));
 
-    public static final RegistryObject<Item> SINGULARITY_BATTERY_INCOMPLETE = ITEMS.register("singularity_battery_incomplete",
-            () -> new SequencedAssemblyItem(new Item.Properties().stacksTo(1)));
+    public static RegistryObject<Item> SINGULARITY_BATTERY_INCOMPLETE;
 
     public static final RegistryObject<Item> SINGULARITY_RIFLE = ITEMS.register("singularity_rifle",
             () -> new SingularityRifle(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
+
+        // Optional Create mod integration
+        if (ModList.get().isLoaded("create")) {
+            registerCreateItems();
+        }
+    }
+
+    private static void registerCreateItems() {
+        try {
+            // Dynamically load class so JVM won’t fail if Create is missing
+            Class<?> seqItemClass = Class.forName("com.simibubi.create.content.processing.sequenced.SequencedAssemblyItem");
+            Item item = (Item) seqItemClass
+                    .getConstructor(Item.Properties.class)
+                    .newInstance(new Item.Properties().stacksTo(1));
+            SINGULARITY_BATTERY_INCOMPLETE = ITEMS.register("singularity_battery_incomplete", () -> item);
+        } catch (Exception e) {
+            ReinforcedBlackHoles.LOGGER.warn("Create detected, but failed to register SequencedAssemblyItem:", e);
+        }
     }
 }
